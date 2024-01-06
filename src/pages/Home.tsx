8d327a37ed3c04e280c4fc75c43f74b72447fa65
Khaled Mohamed Mowafy <45627560+KhaledMowafy@users.noreply.github.com>
2024-01-06 08:23:03 +0200

    fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,6 +6,8 @@ function home() {
         <h1>hello world!</h1>
         <a href="knowticed://signin" className="text-white">Knowticed</a>
         <a href="knowticed://qrCodeView" className="text-white">QR Code View</a>
+        <br/>
+        <a href="knowticed://qrCodeView/0Xvbug093hf3vZ5XLETaMd3WyCk1" className="text-white">USER ID</a>
     </div>
    </>
   )
