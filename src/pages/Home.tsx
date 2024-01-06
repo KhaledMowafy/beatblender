321e42898c4292abff99f42d5d7cdc41048ea3e0
Khaled Mohamed Mowafy <45627560+KhaledMowafy@users.noreply.github.com>
2024-01-06 07:47:08 +0200

    fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,7 @@ function home() {
     <div className="h-screen mt-20 ms-30 bg-[#1B1C1D]">
         <h1>hello world!</h1>
         <a href="knowticed://signin" className="text-white">Knowticed</a>
+        <a href="knowticed://qrCodeView" className="text-white">QR Code View</a>
     </div>
    </>
   )
